5d4f98d3dd1e820f6505d4ea9d17a2da70df15c9
BenJShea <104590782+BenJShea@users.noreply.github.com>
2022-05-01 22:49:17 -0400

    Add files via upload

---
+++ FinalProj.py
@@ -0,0 +1,26 @@
+import math
+
+num1 = -1
+num2  = -1
+total = 0
+squaredDiff = 0
+
+temps = list(map(float, input("Enter 10 temperatures: ").split()))
+
+for i in range(0,10):
+    num1 = num1 + 1
+    total = total + temps[num1]
+
+mean = total / 10
+
+for i in range(0,10):
+    num2 = num2 + 1
+    squaredDiff = squaredDiff + (temps[num2] - mean)**2
+
+stanDeviation = (squaredDiff / 10)**0.5
+
+if stanDeviation <= 1.0:
+    print("COMFY")
+elif stanDeviation > 1.0:
+    print("NOT COMFY")
+
